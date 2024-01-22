5b959aec5a988daa711076c9b194bbf15840cd3b
mariovelandiac <mariovelandiawork@gmail.com>
2024-01-22 16:36:34 -0500

    adding new test case to user's model

--- avanzatech_blog/user/tests/tests_model.py
+++ avanzatech_blog/user/tests/tests_model.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from user.tests.factories import CustomUserFactory
+from team.tests.factories import TeamFactory
 from user.models import CustomUser
 from team.models import Team
 from user.constants import SUPER_USER_TEAM_NAME
@@ -7,11 +8,14 @@ from user.constants import SUPER_USER_TEAM_NAME
 class UserModelTests(TestCase):
 
     def test_create_blogger_user_successfully_in_database(self):
+        # Arrange
+        current_objects = CustomUser.objects.count()
         # Act
         user = CustomUserFactory()
         db_user = CustomUser.objects.get(email=user.email)
         # Assert
         self.assertEqual(user, db_user)
+        self.assertEqual(CustomUser.objects.count(), current_objects + 1)
 
     def test_raw_password_is_not_stored_in_database(self):
         # Arrange
@@ -63,6 +67,15 @@ class UserModelTests(TestCase):
         self.assertIs(db_user.is_staff, False)
         self.assertIs(db_user.is_superuser, False)
         
-
+    def test_users_by_team_can_be_listed(self):
+        # Arrange
+        amount_users_in_team = 10
+        team_instance = TeamFactory()
+        CustomUserFactory.create_batch(amount_users_in_team, team=team_instance)
+        CustomUserFactory.create_batch(5)
+        # Act
+        users_by_team = CustomUser.objects.filter(team_id=team_instance.id).count()
+        # Assert
+        self.assertEqual(users_by_team, amount_users_in_team)
 
     
