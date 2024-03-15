42229d67dfb878b5a612a7fdde0323e727338aaf
mariovelandiac <mariovelandiawork@gmail.com>
2024-03-15 17:33:28 -0500

    creating home page component

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
+import { PostListComponent } from '../../components/post-list/post-list.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent, PostListComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
 export class HomeComponent {
-
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('Home');
+  }
 }
