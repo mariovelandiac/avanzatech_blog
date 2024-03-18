443931a69bbd0c5381bd23ca1983184c5232a785
mariovelandiac <mariovelandiawork@gmail.com>
2024-03-18 09:44:11 -0500

    changing structure to smart and dump component

--- frontend/src/app/components/post-list/post-list.component.ts
+++ frontend/src/app/components/post-list/post-list.component.ts
@@ -5,16 +5,19 @@ import { PostService } from '../../services/post.service';
 import { CommonModule } from '@angular/common';
 import { LikeCounterComponent } from '../like-counter/like-counter.component';
 import { CommentCounterComponent } from '../comment-counter/comment-counter.component';
-import { InteractionBarComponent } from '../interaction-bar/interaction-bar.component';
 import { LikeService } from '../../services/like.service';
 import { CommentService } from '../../services/comment.service';
 import { AuthService } from '../../services/auth.service';
 import { UserStateService } from '../../services/user-state.service';
+import { LikeActionComponent } from '../like-action/like-action.component';
+import { DeleteActionComponent } from '../delete-action/delete-action.component';
+import { CommentActionComponent } from '../comment-action/comment-action.component';
+import { EditActionComponent } from '../edit-action/edit-action.component';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [PostContentComponent, LikeCounterComponent, CommentCounterComponent,InteractionBarComponent, CommonModule],
+  imports: [PostContentComponent, LikeCounterComponent, CommentCounterComponent,LikeActionComponent, CommentActionComponent, EditActionComponent, DeleteActionComponent,CommonModule],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.sass'
 })
@@ -27,20 +30,42 @@ export class PostListComponent implements OnInit {
     private likeService: LikeService,
     private commentService: CommentService,
     private authService: AuthService,
+    private userService: UserStateService
   ) {}
 
   ngOnInit() {
-    this.postService.list().subscribe(posts => {
-      this.posts = posts;
-      this.getLikes();
-      this.getComments();
-    });
     this.authService.isAuthenticated$.subscribe(isAuthenticated => {
       this.isAuthenticated = isAuthenticated;
     });
+    this.postService.list().subscribe(posts => {
+      this.posts = posts;
+      this.getLikedByUser();
+      this.setPermissions();
+      this.getLikes();
+      this.getComments();
+    });
   }
 
-  getLikes() {
+  getLikedByUser(): void {
+    if (!this.isAuthenticated) {
+      return;
+    }
+    const userId = this.userService.getUser().id;
+    for (let post of this.posts) {
+      this.likeService.getLikesByUserAndPost(post.id, userId).subscribe(liked => {
+        post.likedByAuthenticatedUser = liked;
+      });
+    }
+  }
+
+  setPermissions(): void {
+    for (let post of this.posts) {
+      console.log(post.category_permission);
+      console.log(this.userService.getUser().teamId);
+    }
+  }
+
+  getLikes(): void {
     for (let post of this.posts) {
       this.likeService.getLikesByPost(post.id).subscribe(likes => {
         post.likes = likes;
@@ -48,11 +73,12 @@ export class PostListComponent implements OnInit {
     }
   }
 
-  getComments() {
+  getComments(): void {
     for (let post of this.posts) {
       this.commentService.getCommentsByPost(post.id).subscribe(comments => {
         post.comments = comments;
       });
     }
   }
+
 }
