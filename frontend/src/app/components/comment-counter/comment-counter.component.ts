af08c62d87a14aa6fb71f89acd458b7d2c7318fa
mariovelandiac <mariovelandiawork@gmail.com>
2024-03-18 09:42:23 -0500

    adding Loading... message while fetching data

--- frontend/src/app/components/comment-counter/comment-counter.component.ts
+++ frontend/src/app/components/comment-counter/comment-counter.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { CommentListDTO } from '../../models/interfaces/comment.interface';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-comment-counter',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './comment-counter.component.html',
   styleUrl: './comment-counter.component.sass'
 })
@@ -17,9 +18,10 @@ export class CommentCounterComponent implements OnChanges {
   commentPlural = 's';
 
   ngOnChanges(): void {
-    if (this.comments && this.comments.results.length > 0) {
-      this.commentCounter = +this.comments.count;
+    if (this.comments && this.comments.count !== 0) {
+      this.commentCounter = this.comments.count;
       this.commentPlural = this.commentCounter == 1 ? '' : 's ';
+      // Storage post Id for routing in comment action component
       this.postId = this.comments.results[0].post;
     }
   }
--- frontend/src/app/components/like-counter/like-counter.component.ts
+++ frontend/src/app/components/like-counter/like-counter.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { LikeListDTO } from '../../models/interfaces/like.interface';
 import { BaseUser } from '../../models/interfaces/user.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-like-counter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './like-counter.component.html',
   styleUrl: './like-counter.component.sass'
 })
@@ -27,6 +28,8 @@ export class LikeCounterComponent implements OnChanges {
     for (let like of this.likes.results) {
       likedBy.push(like.user.first_name + ' ' + like.user.last_name)
     }
+    // TODO POPUP
+    console.log(likedBy);
   }
 
 }
