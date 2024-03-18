48ef4aa9ea43e1acf2cf57e2e875c75dfd70293b
Mario Velandia Ciendúa <mariovelandiawork@gmail.com>
2024-03-17 19:53:14 -0500

    comment counter component

--- frontend/src/app/components/comment-counter/comment-counter.component.ts
+++ frontend/src/app/components/comment-counter/comment-counter.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { CommentListDTO } from '../../models/interfaces/comment.interface';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-comment-counter',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './comment-counter.component.html',
   styleUrl: './comment-counter.component.sass'
 })
-export class CommentCounterComponent {
+export class CommentCounterComponent implements OnChanges {
+  @Input() comments!: CommentListDTO | undefined
+  private postId!: string;
+  commentCounter = 0;
+  commentPlural = 's';
 
+  ngOnChanges(): void {
+    if (this.comments && this.comments.results.length > 0) {
+      this.commentCounter = +this.comments.count;
+      this.commentPlural = this.commentCounter == 1 ? '' : 's ';
+      this.postId = this.comments.results[0].post;
+    }
+  }
 }
