1b64dfae16e2bb7040b2fbee19870327ff4bdf9a
mariovelandiac <mariovelandiawork@gmail.com>
2024-03-18 17:21:50 -0500

    like updated with like-action component

--- frontend/src/app/components/like-counter/like-counter.component.ts
+++ frontend/src/app/components/like-counter/like-counter.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnChanges } from '@angular/core';
-import { LikeListDTO } from '../../models/interfaces/like.interface';
+import { LikeList, LikeListDTO } from '../../models/interfaces/like.interface';
 import { BaseUser } from '../../models/interfaces/user.interface';
 import { CommonModule } from '@angular/common';
 
@@ -11,7 +11,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './like-counter.component.sass'
 })
 export class LikeCounterComponent implements OnChanges {
-  @Input() likes: LikeListDTO | undefined;
+  @Input() likes: LikeList | undefined;
   likeCounter = 0;
   likePlural = 's';
 
@@ -25,8 +25,8 @@ export class LikeCounterComponent implements OnChanges {
   showLikes(): void {
     const likedBy: string[] = []
     if (!this.likes) return;
-    for (let like of this.likes.results) {
-      likedBy.push(like.user.first_name + ' ' + like.user.last_name)
+    for (let like of this.likes.likedBy) {
+      likedBy.push(like.firstName + ' ' + like.lastName)
     }
     // TODO POPUP
     console.log(likedBy);
