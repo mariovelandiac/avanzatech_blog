8c4c66b034f33a417b074cc5fa4b7e18c0a621ae
mariovelandiac <mariovelandiawork@gmail.com>
2024-03-19 11:32:35 -0500

    edit action redirects to edit page

--- frontend/src/app/components/edit-action/edit-action.component.ts
+++ frontend/src/app/components/edit-action/edit-action.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IconDefinition, faPenToSquare } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-edit-action',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterModule],
   templateUrl: './edit-action.component.html',
   styleUrl: './edit-action.component.sass'
 })
 export class EditActionComponent {
+  @Input() postId!: number;
   editIcon: IconDefinition = faPenToSquare;
+
+  get editUrl(): string {
+    return `/post/edit/${this.postId}`;
+  }
 }
