9c302b18e8e08b3220788b9ebe83ab97f867ea09
mariovelandiac <mariovelandiawork@gmail.com>
2024-03-20 20:56:18 -0500

    detail and edit dump components in post-list testing

--- frontend/src/app/components/comment-action/comment-action.component.spec.ts
+++ frontend/src/app/components/comment-action/comment-action.component.spec.ts
@@ -2,10 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CommentActionComponent } from './comment-action.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 describe('CommentActionComponent', () => {
   let component: CommentActionComponent;
   let fixture: ComponentFixture<CommentActionComponent>;
+  let router: Router
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,10 +17,25 @@ describe('CommentActionComponent', () => {
 
     fixture = TestBed.createComponent(CommentActionComponent);
     component = fixture.componentInstance;
+    component.postId = 1;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should navigate to post detail page', () => {
+    // Arrange
+    const element = fixture.nativeElement.querySelector('fa-icon');
+    const expectedRoute = component.postDetailRoute;
+    const spy = spyOn(router, 'navigate');
+    const navigateByUrlSpy = spyOn(router, 'navigateByUrl') as jasmine.Spy;
+    // Act
+    element.click();
+    // Assert
+    const navArgs = navigateByUrlSpy.calls.first().args[0];
+    expect(router.navigateByUrl).toHaveBeenCalled();
+    expect(expectedRoute).toContain(navArgs);
   });
 });
--- frontend/src/app/components/edit-action/edit-action.component.spec.ts
+++ frontend/src/app/components/edit-action/edit-action.component.spec.ts
@@ -4,6 +4,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
+import { concatMap } from 'rxjs';
 
 describe('EditActionComponent', () => {
   let component: EditActionComponent;
@@ -19,11 +20,26 @@ describe('EditActionComponent', () => {
 
     fixture = TestBed.createComponent(EditActionComponent);
     component = fixture.componentInstance;
+    component.postId = 1;
     router = TestBed.inject(Router);
     fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to edit page', () => {
+    // Arrange
+    const element = fixture.nativeElement.querySelector('fa-icon');
+    const expectedRoute = component.editUrl;
+    const spy = spyOn(router, 'navigate');
+    const navigateByUrlSpy = spyOn(router, 'navigateByUrl') as jasmine.Spy;
+    // Act
+    element.click();
+    // Assert
+    const navArgs = navigateByUrlSpy.calls.first().args[0];
+    expect(router.navigateByUrl).toHaveBeenCalled();
+    expect(expectedRoute).toContain(navArgs);
   });
 });
