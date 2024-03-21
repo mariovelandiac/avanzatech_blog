6f3ec9f109f2fde5298a6e525f2066c32306f101
mariovelandiac <mariovelandiawork@gmail.com>
2024-03-20 19:03:40 -0500

    like mocking

---
+++ frontend/src/app/test-utils/like.model.mock.ts
@@ -0,0 +1,82 @@
+import { LikeList } from "../models/interfaces/like.interface";
+
+export const mockLikeList: LikeList = {
+  count: 15,
+  likedBy: [
+    {
+      id: 1,
+      firstName: 'John',
+      lastName: 'Doe'
+    },
+    {
+      id: 2,
+      firstName: 'Jane',
+      lastName: 'Doe'
+    },
+    {
+      id: 3,
+      firstName: 'John',
+      lastName: 'Smith'
+    },
+    {
+      id: 4,
+      firstName: 'Jane',
+      lastName: 'Smith'
+    },
+    {
+      id: 5,
+      firstName: 'Michael',
+      lastName: 'Johnson'
+    },
+    {
+      id: 6,
+      firstName: 'Emily',
+      lastName: 'Davis'
+    },
+    {
+      id: 7,
+      firstName: 'David',
+      lastName: 'Brown'
+    },
+    {
+      id: 8,
+      firstName: 'Olivia',
+      lastName: 'Miller'
+    },
+    {
+      id: 9,
+      firstName: 'Daniel',
+      lastName: 'Wilson'
+    },
+    {
+      id: 10,
+      firstName: 'Sophia',
+      lastName: 'Anderson'
+    },
+    {
+      id: 11,
+      firstName: 'Matthew',
+      lastName: 'Taylor'
+    },
+    {
+      id: 12,
+      firstName: 'Ava',
+      lastName: 'Thomas'
+    },
+    {
+      id: 13,
+      firstName: 'Andrew',
+      lastName: 'Harris'
+    },
+    {
+      id: 14,
+      firstName: 'Emma',
+      lastName: 'Clark'
+    },
+    {
+      id: 15,
+      firstName: 'Joseph',
+      lastName: 'Lewis'
+    }
+  ]
+}
